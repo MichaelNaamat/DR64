b9922c8e3e4bc4011f43df76941086deb873a31f
Michael Naamat <michael.naamat@gmail.com>
2026-08-18 11:30:55 +0300

    All scripts are moved one level up

---
+++ Scripts/temp_sens_int_test.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+# --->>> Define color variables
+declare -r C_RED='\e[1;31m'
+declare -r C_GREEN='\e[1;32m'
+declare -r C_YELLOW='\e[1;33m'
+declare -r C_BLUE='\e[1;34m'
+declare -r C_NONE='\e[0m' # No Color / Reset
+
+# --->>> Register definition
+declare -r REG_TEMP="0x00"
+declare -r REG_CR="0x01"
+declare -r REG_TLOW="0x02"
+declare -r REG_THIGH="0x03"
+declare -r REG_OS="0x04"
+
+# --->>> List of temperature chips (i2c bus & device)
+i2c_bus=("0x00" "0x04" "0x04" "0x04" "0x04")
+i2c_dev=("0x49" "0x48" "0x49" "0x4A" "0x4C")
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Print values of Temp, T-Low, T-High registers for a given device
+# Parameters:
+# $1 - Bus number
+# $2 - Device number
+# Return: None
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function read_temp_reg()
+{
+    local bus dev cur_temp t_low t_high 
+    bus=$1
+    dev=$2
+
+    cur_temp=$(i2cget -y -f $bus $dev $REG_TEMP w)
+    t_low=$(i2cget -y -f $bus $dev $REG_TLOW w)
+    t_high=$(i2cget -y -f $bus $dev $REG_THIGH w)
+    echo "Current: Temp=$cur_temp, T-Low=$t_low, T-High=$t_high"
+}
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Test interrupt line for temp75b-q1 tempratur sensor 
+# Parameters:
+# $1 - Bus number
+# $2 - Device number
+# Return: None
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function check_temp75b_int()
+{
+    local bus dev cur_temp org_t_low org_t_high 
+    bus=$1
+    dev=$2
+	
+    # ---->>> Save original High/Low limit to restore at end of Test
+    org_t_low=$(i2cget -y -f $bus $dev $REG_TLOW w)
+    org_t_high=$(i2cget -y -f $bus $dev $REG_THIGH w)
+	
+    echo "*****************************************"
+    echo "* Testing Temp Dev $dev on i2c bus $bus"
+    echo "*****************************************"
+	
+    # --->>> Set Configuration Reg (0x01).:
+    # OS (15)    = '0'
+    # CR (13-14) = '00'  | 37Hz conversion rate (typ) (default)
+    # FQ (11-12) = '00'  | 1 fault (default)
+    # POL (10)   = '0'   | ALERT is active low (default)
+    # TM  (9)    = '1'   | ALERT is in interrupt mode
+    # SD  (8)    = '0'   | Device is in continuous conversion mode (default)
+    # Mask: 0000 0010 = 0x02
+    i2cset -y -f $bus $dev $REG_CR 0x02
+      
+    # --->>> Read current temperature from device (Addr=0x00) & High/Low limits
+    read_temp_reg $bus $dev     # DEBUG & clear int line
+    
+    # --->>> Print int state
+    gpio_line=$(cat  /sys/kernel/debug/gpio | grep "PC_04")
+    echo "Pre-test Int state: ${gpio_line:58:2}"
+
+    # ============ T-High test: Set T-High to +5c (T-Low stay -40c)
+    i2cset -y -f $bus $dev $REG_THIGH 0x0005 w
+
+    # --->>> Print int state
+    gpio_line=$(cat  /sys/kernel/debug/gpio | grep "PC_04")
+    echo -n "T-High Int state(${gpio_line:58:2}): "
+    test "${gpio_line:58:2}" = "lo" && echo "Pass" || echo "*** FAIL"
+    read_temp_reg $bus $dev     # DEBUG & clear int line
+
+    # ============ T-Low test: Set T-Low to +60c & T-High to +65c)
+    i2cset -y -f $bus $dev $REG_TLOW 0x003C w
+    i2cset -y -f $bus $dev $REG_THIGH 0x0041 w
+
+    # --->>> Print int state
+    gpio_line=$(cat  /sys/kernel/debug/gpio | grep "PC_04")
+    echo -n "T-Low Int state(${gpio_line:58:2}): "
+    test "${gpio_line:58:2}" = "lo" && echo "Pass" || echo "*** FAIL"
+
+    # --->>> Restore original values of T-Low/T-High before moving to next device
+    # DEBUG: Set T-High: +60c, T-Low: -40c
+#    t_high="0x003C"
+#    t_low="0x00D8"
+
+    echo "Restoring Org T-Low: $t_low T-High: $t_high"
+    i2cset -y -f $bus $dev $REG_TLOW $org_t_low w
+    i2cset -y -f $bus $dev $REG_THIGH $org_t_high w 
+    read_temp_reg $bus $dev     # DEBUG & clear int line
+
+    # --->>> Re-test interrupt after restoring limits to normal values
+    gpio_line=$(cat  /sys/kernel/debug/gpio | grep "PC_04")
+    echo "Post-test Int state: ${gpio_line:58:2}"
+}
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Test interrupt line for temp75c-q1 tempratur sensor 
+# Parameters:
+# $1 - Bus number
+# $2 - Device number
+# Return: None
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function check_temp75c_int()
+{
+    local bus dev t_low t_high
+    bus=$1
+    dev=$2
+    
+    echo "*****************************************"
+    echo "* Testing Temp Dev $dev on i2c bus $bus"
+    echo "*****************************************"
+	
+    # --->>> Set Configuration Reg (0x01).:
+    #  - OS  =  '1': Set 'One-Shot' mode 
+    #  - FQ  = '00': Trigger ALERT adter 1 fault (defult)
+    #  - POL =  '0': ALERT active Low (default)
+    #  - TM  =  '1': ALERT in interrupt mode
+    #  - SD  =  '0': Clear 'Shutdown Mode' 
+    #  xx10 0010 = 0x22
+    i2cset -y -f $bus $dev $REG_CR 0x22
+    
+    # --->>> Write 'One-Shot' register to start conversion (Addr=0x04) (suppress errors)
+    i2cset -y -f $bus $dev $REG_OS 0x00 >/dev/null 2>&1
+    
+    # --->>> Read current temperature from device (Addr=0x00)
+    echo -n "Current Temp: "
+    i2cget -y -f $bus $dev $REG_TEMP w
+    
+    # --->>> Read original values of T-Low/T-High for restoration after test
+    t_low=$(i2cget -y -f $bus $dev 0x02 w)
+    t_high=$(i2cget -y -f $bus $dev 0x03 w)
+	
+    # --->>> Set T-Low Temp limit (Addr 0x02) to 1c
+    i2cset -y -f $bus $dev $REG_TLOW 0x0001 w
+    
+    # --->>> Set T-High Temp limit (Addr 0x03) to 5c
+    i2cset -y -f $bus $dev $REG_HIGH 0x0005 w
+    
+    # --->>> Write 'One-Shot' register to start conversion (Addr=0x04) (suppress errors)
+    i2cset -y -f $bus $dev $REG_OS 0x00>/dev/null 2>&1
+    
+    # --->>> Re-read values of T-Low/T-High
+    echo -n "T-Low: "
+    i2cget -y -f $bus $dev $REG_TLOW w
+    
+    echo -n "T-High: "
+    i2cget -y -f $bus $dev $REG_THIGH w
+    
+    # --->>> Read GPIO to see if interrupt is activated
+    # "gpio-357 (PC_04               |AViVA               ) in  lo IRQ"
+    gpio_line=$(cat  /sys/kernel/debug/gpio | grep "PC_04")
+    echo "Int state: ${gpio_line:58:2}"
+    test "${gpio_line:58:2}" = "lo" && echo -e "${C_GREEN}>>> Test Pass${C_NONE}" || echo -e "${C_RED}*** Test FAIL ***${C_NONE}"
+
+    # --->>> Restore original values of T-Low/T-High before moving to next device
+    # DEBUG: Set T-High: 60c, T-Low: 58c
+    t_high="0x003C"
+    t_low="0x003A"
+
+    echo "Restoring Org T-Low: $t_low T-High: $t_high"
+    i2cset -y -f $bus $dev $REG_TLOW $t_low w
+    i2cset -y -f $bus $dev $REG_THIGH $t_high w 
+
+    # --->>> Re-test interrupt after restoring limits to normal values
+    i2cset -y -f $bus $dev $REG_OS 0x00>/dev/null 2>&1
+    gpio_line=$(cat  /sys/kernel/debug/gpio | grep "PC_04")
+    echo "Normal Int state: ${gpio_line:58:2}"
+}
+
+# ---->>>> Call tests for all devices...
+for i in {0..4}
+do
+    check_temp75b_int ${i2c_bus[$i]} ${i2c_dev[$i]}
+done
+
---
+++ Scripts/voltage_mon_test.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+# --->>> Define color variables. TODO: Move to common script for all tests
+# Normal colors
+declare -r C_RED='\e[0;31m'
+declare -r C_GREEN='\e[0;32m'
+declare -r C_YELLOW='\e[0;33m'
+declare -r C_BLUE='\e[0;34m'
+
+# Bold colors
+declare -r C_RED_B='\e[1;31m'
+declare -r C_GREEN_B='\e[1;32m'
+declare -r C_YELLOW_B='\e[1;33m'
+declare -r C_BLUE_B='\e[1;34m'
+
+# Underline colors
+declare -r C_RED_U='\e[4;31m'
+declare -r C_GREEN_U='\e[4;32m'
+declare -r C_YELLOW_U='\e[4;33m'
+declare -r C_BLUE_U='\e[4;34m'
+
+declare -r C_NONE='\e[0m' # No Color / Reset
+
+# -------------------- Bank independent ------------------------
+declare -r REG_BANK_SEL="0xF0"
+
+# -------------------- Bank 0 registers ------------------------
+# --->>> Reg addresses of MON_LVL[i] for channels 0..7
+declare -a VMON_LVL_REG=("0x40" "0x41" "0x42" "0x43" "0x44" "0x45" "0x46" "0x47")   
+
+# -------------------- Bank 1 registers ------------------------
+declare -r REG_VRANGE_MUL="0x1F"   
+
+
+declare -A U93_ch1=(name "0.875V (core) +2.5% / -3%" min 0.84875 typ 0.875  max 0.896875)
+declare -A U93_ch2=(name "1.8V (PCIE) +/-5%"         min 1.71    typ 1.8    max 1.89)
+declare -A U93_ch3=(name "1.8V (IP) +/-3%"           min 1.746   typ 1.8    max 1.854)
+declare -A U93_ch4=(name "0.86V (PCIE) +/-5%"        min 0.817   typ 0.86   max 0.903)
+declare -A U93_ch5=(name "0.75V (MIPI) +/-5%"        min 0.7125  typ 0.75   max 0.7875)
+declare -A U93_ch6=(name "1.05V (DDR) +/-3%"         min 1.0185  typ 1.05   max 1.0815)
+declare -A U93_ch7=(name "0.5V (DDR) +/-5%"          min 0.475   typ 0.5    max 0.525)
+declare -A U93_ch8=(name "1.8V (Main) +/-5%"         min 1.71    typ 1.8    max 1.89)
+declare -a U93_channels=(U93_ch1 U93_ch2 U93_ch3 U93_ch4 U93_ch5 U93_ch6 U93_ch7 U93_ch8)
+
+declare -A U94_ch1=(name "0.875V (core) +2.5% / -3%" min 0.84875 typ 0.875  max 0.896875)
+declare -A U94_ch2=(name "1.8V (PCIE) +/-5%"         min 1.71    typ 1.8    max 1.89)
+declare -A U94_ch3=(name "1.8V (IP) +/-3%"           min 1.746   typ 1.8    max 1.854)
+declare -A U94_ch4=(name "0.86V (PCIE) +/-5%"        min 0.817   typ 0.86   max 0.903)
+declare -A U94_ch5=(name "0.75V (MIPI) +/-5%"        min 0.7125  typ 0.75   max 0.7875)
+declare -A U94_ch6=(name "1.05V (DDR) +/-3%"         min 1.0185  typ 1.05   max 1.0815)
+declare -A U94_ch7=(name "0.5V (DDR) +/-5%"          min 0.475   typ 0.5    max 0.525)
+declare -A U94_ch8=(name "1.8V (Main) +/-5%"         min 1.71    typ 1.8    max 1.89)
+declare -a U94_channels=(U94_ch1 U94_ch2 U94_ch3 U94_ch4 U94_ch5 U94_ch6 U94_ch7 U94_ch8)
+
+declare -A U95_ch1=(name "1.1V (USS_12v_M) +/-5%"   min 1.03835 typ 1.1     max 1.14765)
+declare -A U95_ch2=(name "5V (5V_CAN) +/-5%"        min 4.75    typ 5       max 5.25)
+declare -A U95_ch3=(name "1.2V (DES_1v2) +/-5%"     min 1.14    typ 1.2     max 1.26)
+declare -A U95_ch4=(name "1.2V (5V_AOLDO_M) +/-5%"  min 1.12575 typ 1.185   max 1.24425)
+declare -A U95_ch5=(name "0.8V (DES_0V8) +/-5%"     min 0.76    typ 0.8     max 0.84)
+declare -A U95_ch6=(name "3.3V (3V3) +/-5%"         min 3.135   typ 3.3     max 3.465)
+declare -A U95_ch7=(name "1.1V (12vPoC_M) +/-5%"    min 1.03835 typ 1.1     max 1.14765)
+declare -A U95_ch8=(name "5V"                       min 4.75    typ 5       max 5.25)
+declare -a U95_channels=(U95_ch1 U95_ch2 U95_ch3 U95_ch4 U95_ch5 U95_ch6 U95_ch7 U95_ch8)
+    
+declare -A U114_ch1=(name "0.8V () +/-5%"           min 0.76    typ 0.8  max 0.84)
+declare -A U114_ch2=(name "1.2V () +/-5%"           min 1.14    typ 1.2  max 1.26)
+declare -A U114_ch3=(name "0.8V () +/-5%"           min 0.76    typ 0.8  max 0.84)
+declare -A U114_ch4=(name "1.2V () +/-5%"           min 1.14    typ 1.2  max 1.26)
+declare -A U114_ch5=(name "0.84V () +/-5%"          min 0.798   typ 0.84 max 0.882)
+declare -A U114_ch6=(name "1.2V () +/-5%"           min 1.14    typ 1.2  max 1.26)
+declare -A U114_ch7=(name "1.8V (ETH_1v8) +/-5%"    min 1.71    typ 1.8  max 1.89)
+declare -A U114_ch8=(name "5V (Main 5V) +/-5%"      min 4.75    typ 5    max 5.25)
+declare -a U114_channels=(U114_ch1 U114_ch2 U114_ch3 U114_ch4 U114_ch5 U114_ch6 U114_ch7 U114_ch8)
+    
+declare -A U148_ch1=(name "0.8V (TDA4_0v8_VDD_CORE) +/-5%"   min 0.76    typ 0.8     max 0.84)
+declare -A U148_ch2=(name "0.8V (TDA4_0v8_CPU_AVS) +/-5%"    min 0.76    typ 0.8     max 0.84)
+declare -A U148_ch3=(name "1.1V (TDA4_1v1) +/-5%"            min 1.045   typ 1.1     max 1.155)
+declare -A U148_ch4=(name "0.85V (TDA4_0v85) +/-5%"          min 0.8075  typ 0.85    max 0.8925)
+declare -A U148_ch5=(name "1.8V (TDA4_1v8_PHY_LDO) +/-5%"    min 1.71    typ 1.8     max 1.89)
+declare -A U148_ch6=(name "0.8V (TDA4_0v8_DLL_LDO) +/-5%"    min 0.76    typ 0.8     max 0.84)
+declare -A U148_ch7=(name "1.8V (TDA4_1v8_PLL_LDO) +/-5%"    min 1.71    typ 1.8     max 1.89)
+## declare -A U148_ch8=(name "0V disabled"                      min 0       typ 0       max 0)
+declare -a U148_channels=(U148_ch1 U148_ch2 U148_ch3 U148_ch4 U148_ch5 U148_ch6 U148_ch7 U148_ch8)
+
+# ----->>> Define VMON chips and their channels
+declare -A vmon_chip_U93=( name "U93"  bus "0x00" dev "0x37" channels U93_channels)
+declare -A vmon_chip_U94=( name "U94"  bus "0x00" dev "0x36" channels U94_channels)
+declare -A vmon_chip_U95=( name "U95"  bus "0x00" dev "0x35" channels U95_channels)
+declare -A vmon_chip_U114=(name "U114" bus "0x00" dev "0x34" channels U114_channels)
+declare -A vmon_chip_U148=(name "U148" bus "0x00" dev "0x33" channels U148_channels)  # ** Conflict on bus number
+
+vmon_chip=(vmon_chip_U93 vmon_chip_U94 vmon_chip_U95 vmon_chip_U114 vmon_chip_U148)
+
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Test values of voltage monitor registers for a given device
+# Parameters:
+# $1 - Bus number
+# $2 - Device number
+# $3 - Monitor channel (0..7)
+# $4 - Channel information (name, min, typ, max)
+# Return: None
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function vmon_check_channel()
+{
+    local bus dev ch
+    local ch_info
+    local mon_lvl volt_val
+    local min_val typ_val max_val
+
+    bus=$1
+    dev=$2
+    ch=$3
+    declare -n ch_info="$4"
+    min_val=${ch_info[min]}
+    typ_val=${ch_info[typ]}
+    max_val=${ch_info[max]}
+
+    # ---->>>> read monitor level for channel $ch
+    mon_lvl=$(i2cget -y -f $bus $dev ${VMON_LVL_REG[$ch]})      
+    mon_lvl=$(($mon_lvl))
+#    mon_lvl=$((0x00))  # For testing, assume max level
+
+    if (( $mon_lvl <= 0 || $mon_lvl > 255 )); then
+        echo -e "${C_RED_B}  >>> ERROR: Invalid monitor level ($mon_lvl) for channel ${ch}${C_NONE}"
+        return
+    fi
+
+    # ---->>> Calculate scaling factor for voltage range according to Max value of channel 
+    if (( $(awk -v x="${ch_info[max]}" 'BEGIN { print (x > 1.475) }') )); then
+        volt_val=$(awk -v a="$mon_lvl" 'BEGIN { printf "%.3f", 0.8 + a * 0.02 }')
+    else
+        volt_val=$(awk -v a="$mon_lvl" 'BEGIN { printf "%.3f", 0.2 + a * 0.005 }')
+    fi
+    echo "  Channel ${ch_info[name]}: min=$min_val, typ=$typ_val, max=$max_val MON_LVL=$mon_lvl, VOLT_VAL=$volt_val"
+
+    # ---->>> Check if voltage value is within min/max range
+    if (( $(awk -v x="$volt_val" -v min="$min_val" 'BEGIN { print (x < min) }') )); then
+        echo -e "${C_RED}  >>> ERROR: Less than min ($volt_val < $min_val)${C_NONE}"
+    elif (( $(awk -v x="$volt_val" -v max="$max_val" 'BEGIN { print (x > max) }') )); then
+        echo -e "${C_RED}  >>> ERROR: More than max ($volt_val > $max_val)${C_NONE}"
+    else
+        echo -e "${C_GREEN}  >>> OK: $volt_val is within range [$min_val, $max_val]${C_NONE}"
+    fi
+
+}
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Do test on VMON device
+# Parameters:
+# $1 - VMON chip information (name, bus, dev)
+# Return: None
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function vmon_check_dev()
+{
+    local bus dev chlist ch_ind ch
+    declare -n chip_info="$1"
+
+    bus=${chip_info[bus]}
+    dev=${chip_info[dev]}
+    declare -n chlist=${chip_info[channels]}
+
+    echo -e "\n****************************************************"
+    echo "* Testing $1: Addr $dev on i2c bus $bus"
+    echo "****************************************************"
+  
+    # --->>> Loop over channels and test each one
+    ch_ind=0
+    for ch in "${chlist[@]}"; do
+        declare -n vmon_ch="$ch"
+        if [[ -z ${vmon_ch[name]} ]]; then   # Skip non-active channels (e.g. U148_ch8)
+            continue
+        fi
+
+        vmon_check_channel "$bus" "$dev" "$ch_ind" "$ch"
+        ch_ind=$((ch_ind + 1))
+    done
+}
+
+########################################################################################
+# ---->>>> Call tests for all devices...
+for chip in "${vmon_chip[@]}"; do
+    vmon_check_dev "$chip"
+done    
